9672b60ac073756e994984c4b5029a7c6ed7d11a
Stephen Njuguna Njambi <stepehen.jambi@student.moringaschool.com>
2022-10-25 15:56:13 +0300

    Completed Lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+    #writter and reader
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+    def match(string)
+        string.select do |text|
+            text.split("").sort == word.split("").sort
+        end
+    end
+end
